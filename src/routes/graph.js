da44819ed5714ecf981d1f4bcdeec821d3beac20
Jacob Bare <jacob@limit0.io>
2018-04-04 10:09:51 -0500

    Use Tenant instance in graph route

--- src/routes/graph.js
+++ src/routes/graph.js
@@ -1,29 +1,10 @@
 const { Router } = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress } = require('apollo-server-express');
+const Tenant = require('../classes/tenant');
 const schema = require('../graph/schema');
 
 const router = Router();
-
-const tenants = [
-  'cygnus_ofcr',
-  'cygnus_fhc',
-  'cygnus_vspc',
-  'cygnus_mprc',
-  'acbm_ooh',
-  'acbm_fcp',
-  'acbm_sdce',
-  'acbm_fl',
-  'acbm_dmn',
-  'indm_ien',
-  'scomm_nvs',
-  'scomm_cltampa',
-  'scomm_cb',
-  'scomm_wcp',
-  'scomm_kcp',
-  'scomm_nvp',
-  'as3_baseplatform',
-];
 
 const mutations = [
   'Website',
@@ -34,11 +15,9 @@ const mutations = [
 router.use(
   bodyParser.json(),
   graphqlExpress((req) => {
-    const tenant = req.get('X-Tenant-Key');
+    const tenant = new Tenant(req.get('X-Tenant-Key'));
     let mutation = req.get('X-Mutation-Type');
     if (!mutations.includes(mutation)) mutation = 'Website';
-
-    if (!tenants.includes(tenant)) throw new Error(`No tenant found for '${tenant}'`);
     return {
       schema,
       context: { auth: { }, tenant, mutation },
