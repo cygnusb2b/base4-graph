9a240bf48edcd5a6ac095a5aa9d6b47e3b9ecf29
Jacob Bare <jacob@limit0.io>
2018-05-29 08:16:36 -0500

    Add authenticate middleware to graph routes

--- src/routes/graph.js
+++ src/routes/graph.js
@@ -1,10 +1,19 @@
 const { Router } = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const { graphqlExpress } = require('apollo-server-express');
 const Tenant = require('../classes/tenant');
+const Credentials = require('../auth/credentials');
 const schema = require('../graph/schema');
 
 const router = Router();
+
+const authenticate = (req, res, next) => {
+  passport.authenticate('bearer', { session: false }, (err, key) => {
+    req.auth = new Credentials({ key, err });
+    next();
+  })(req, res, next);
+};
 
 const mutations = [
   'Website',
@@ -13,14 +22,16 @@ const mutations = [
 ];
 
 router.use(
+  authenticate,
   bodyParser.json(),
   graphqlExpress((req) => {
+    const { auth } = req;
     const tenant = new Tenant(req.get('X-Tenant-Key'));
     let mutation = req.get('X-Mutation-Type');
     if (!mutations.includes(mutation)) mutation = 'Website';
     return {
       schema,
-      context: { auth: { }, tenant, mutation },
+      context: { auth, tenant, mutation },
     };
   }),
 );
