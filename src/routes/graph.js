a5bf48b01aa7014ab3a2182d156eb7c7d9d72096
Jacob Bare <jacob@limit0.io>
2018-08-29 15:02:23 -0500

    Pass base4 instance to graph context

--- src/routes/graph.js
+++ src/routes/graph.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const passport = require('passport');
+const Base4 = require('../base4');
+const db = require('../connections/base4');
 const Tenant = require('../classes/tenant');
 const Credentials = require('../auth/credentials');
 const schema = require('../graph/schema');
@@ -26,10 +28,20 @@ const server = new ApolloServer({
   playground: false,
   context: ({ req }) => {
     const { auth } = req;
+
+    // @todo Remove tenant call and mutation stuff from here...
     const tenant = new Tenant(req.get('X-Tenant-Key'));
     let mutation = req.get('X-Mutation-Type');
     if (!mutations.includes(mutation)) mutation = 'Website';
-    return { auth, tenant, mutation };
+
+    // @todo But keep this...
+    const base4 = new Base4({ db, tenantKey: req.get('X-Tenant-Key') });
+    return {
+      auth,
+      base4,
+      tenant,
+      mutation,
+    };
   },
 });
 server.applyMiddleware({ app: router, path: '/' });
