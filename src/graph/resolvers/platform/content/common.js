bbca54f0a9c37bd1eec3fcf43a1547937a9f946a
Jacob Bare <jacob@limit0.io>
2018-08-29 15:49:06 -0500

    Add seo fields common content resolvers

--- src/graph/resolvers/platform/content/common.js
+++ src/graph/resolvers/platform/content/common.js
@@ -1,3 +1,9 @@
+const { extractMutationValue } = require('../../../../base4');
+
 module.exports = {
   id: doc => doc._id,
+  alias: doc => extractMutationValue(doc, 'Website', 'alias'),
+  slug: doc => extractMutationValue(doc, 'Website', 'slug'),
+  seoTitle: doc => extractMutationValue(doc, 'Website', 'seoTitle') || doc.name,
+  redirects: doc => extractMutationValue(doc, 'Website', 'redirects') || [],
 };
