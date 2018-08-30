90e2975ce5e0d4ac372e836430462652c520c292
Jacob Bare <jacob@limit0.io>
2018-08-30 14:02:03 -0500

    Remove utils and update graph context

--- src/routes/graph.js
+++ src/routes/graph.js
@@ -3,7 +3,6 @@ const { ApolloServer } = require('apollo-server-express');
 const passport = require('passport');
 const Base4 = require('../base4');
 const db = require('../connections/base4');
-const Tenant = require('../classes/tenant');
 const Credentials = require('../auth/credentials');
 const schema = require('../graph/schema');
 
@@ -17,31 +16,13 @@ const authenticate = (req, res, next) => {
 };
 router.use(authenticate);
 
-const mutations = [
-  'Website',
-  'Magazine',
-  'Email',
-];
-
 const server = new ApolloServer({
   schema,
   playground: false,
   context: ({ req }) => {
     const { auth } = req;
-
-    // @todo Remove tenant call and mutation stuff from here...
-    const tenant = new Tenant(req.get('X-Tenant-Key'));
-    let mutation = req.get('X-Mutation-Type');
-    if (!mutations.includes(mutation)) mutation = 'Website';
-
-    // @todo But keep this...
     const base4 = new Base4({ db, tenantKey: req.get('X-Tenant-Key') });
-    return {
-      auth,
-      base4,
-      tenant,
-      mutation,
-    };
+    return { auth, base4 };
   },
 });
 server.applyMiddleware({ app: router, path: '/' });
