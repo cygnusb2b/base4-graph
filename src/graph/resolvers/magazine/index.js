ada08660f8b57ae1fe9a969ddf50919f72071120
Jacob Bare <jacob@limit0.io>
2018-08-30 12:56:24 -0500

    Complete magazine issue resolvers

--- src/graph/resolvers/magazine/index.js
+++ src/graph/resolvers/magazine/index.js
@@ -1,9 +1,11 @@
 const deepAssign = require('deep-assign');
 
+const issue = require('./issue');
 const productPublication = require('./product-publication');
 const section = require('./section');
 
 module.exports = deepAssign(
+  issue,
   productPublication,
   section,
 );
---
+++ src/graph/resolvers/magazine/issue.js
@@ -0,0 +1,33 @@
+const coreResolvers = require('../core');
+
+const { isArray } = Array;
+
+module.exports = {
+  /**
+   *
+   */
+  MagazineIssue: {
+    ...coreResolvers,
+    publication: (issue, _, { base4 }) => base4.reference('platform', 'Product', issue.publication, { type: 'Publication' }),
+    sections: (issue, _, { base4 }) => base4.inverse('magazine', 'Section', 'issue.$id', issue._id),
+    coverImage: (issue, _, { base4 }) => base4.reference('platform', 'Asset', issue.coverImage),
+    redirects: (section) => {
+      const { redirects } = section;
+      return isArray(redirects) ? redirects : [];
+    },
+  },
+
+  /**
+   *
+   */
+  Query: {
+    /**
+     *
+     */
+    magazineIssue: async (_, { input }, { auth, base4 }) => {
+      auth.check();
+      const { id } = input;
+      return base4.strictFindById('magazine', 'Issue', id);
+    },
+  },
+};
