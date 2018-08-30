0a085659ce625cab48812b6d8d3f7d10372a9419
Jacob Bare <jacob@limit0.io>
2018-08-30 13:31:47 -0500

    Complete email product resolvers

--- src/graph/resolvers/email/product.js
+++ src/graph/resolvers/email/product.js
@@ -1,3 +1,7 @@
+const productResolvers = require('../platform/product/common');
+
+const { isArray } = Array;
+
 module.exports = {
   /**
    *
@@ -11,6 +15,38 @@ module.exports = {
       if (obj.type === 'Newsletter') return 'EmailProductNewsletter';
       if (obj.type === 'ThirdParty') return 'EmailProductThirdParty';
       return null;
+    },
+  },
+
+  /**
+   *
+   */
+  EmailProductNewsletter: {
+    ...productResolvers,
+    defaultTesters: ({ defaultTesters }) => (isArray(defaultTesters) ? defaultTesters : []),
+    parent: (deployment, _, { base4 }) => base4.reference('platform', 'Product', deployment.parent, { type: 'Newsletter' }),
+    sections: (deployment, _, { base4 }) => base4.inverse('email', 'Section', 'deployment.$id', deployment._id),
+  },
+
+  /**
+   *
+   */
+  EmailProductThirdParty: {
+    ...productResolvers,
+    defaultTesters: ({ defaultTesters }) => (isArray(defaultTesters) ? defaultTesters : []),
+  },
+
+  /**
+   *
+   */
+  Query: {
+    /**
+     *
+     */
+    emailProductNewsletter: async (_, { input }, { auth, base4 }) => {
+      auth.check();
+      const { id } = input;
+      return base4.strictFindById('platform', 'Product', id, { type: 'Newsletter' });
     },
   },
 };
