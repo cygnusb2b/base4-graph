8bc95a51d0b6b4cd5ba80832138cf759ab89a4e0
Jacob Bare <jacob@limit0.io>
2018-08-31 11:00:12 -0500

    Add image connection and content.images pagin

--- src/graphql/resolvers/platform/asset.js
+++ src/graphql/resolvers/platform/asset.js
@@ -1,6 +1,21 @@
 const { extractMutationValue } = require('../../../base4');
+const paginationResolvers = require('../../../pagination/resolvers');
 
 module.exports = {
+  /**
+   *
+   */
+  PlatformAssetImageConnection: paginationResolvers,
+
+  /**
+   *
+   */
+  PlatformAssetImage: {
+    id: doc => doc._id,
+    approvedWeb: doc => extractMutationValue(doc, 'Website', 'approved'),
+    approvedMagazine: doc => extractMutationValue(doc, 'Magazine', 'approved'),
+  },
+
   /**
    *
    */
@@ -13,14 +28,5 @@ module.exports = {
       const { id } = input;
       return base4.strictFindById('platform', 'Asset', id, { type: 'Image' });
     },
-  },
-
-  /**
-   *
-   */
-  PlatformAssetImage: {
-    id: doc => doc._id,
-    approvedWeb: doc => extractMutationValue(doc, 'Website', 'approved'),
-    approvedMagazine: doc => extractMutationValue(doc, 'Magazine', 'approved'),
   },
 };
