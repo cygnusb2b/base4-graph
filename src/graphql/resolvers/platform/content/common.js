c54295a93089813b8af406cdab7b5bd23374fe89
Jacob Bare <jacob@limit0.io>
2018-09-01 11:55:24 -0500

    Use directive for primaryImage

--- src/graphql/resolvers/platform/content/common.js
+++ src/graphql/resolvers/platform/content/common.js
@@ -1,6 +1,3 @@
-const { extractMutationValue } = require('../../../../base4');
-
 module.exports = {
-  primaryImage: (doc, _, { base4 }) => base4.reference('platform', 'Asset', doc.primaryImage),
   taxonomy: (doc, _, { base4 }) => base4.references('platform', 'Taxonomy', doc.taxonomy),
 };
