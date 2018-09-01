0c9e5601eeca25459c26553a006d5c958052bd1c
Jacob Bare <jacob@limit0.io>
2018-09-01 14:47:10 -0500

    Add directives and inputs to email product

--- src/graphql/resolvers/email/product.js
+++ src/graphql/resolvers/email/product.js
@@ -1,16 +1,11 @@
 const productResolvers = require('../platform/product/common');
 
-const { isArray } = Array;
-
 module.exports = {
   /**
    *
    */
   EmailProductNewsletter: {
     ...productResolvers,
-    defaultTesters: ({ defaultTesters }) => (isArray(defaultTesters) ? defaultTesters : []),
-    parent: (deployment, _, { base4 }) => base4.reference('platform', 'Product', deployment.parent, { type: 'Newsletter' }),
-    sections: (deployment, _, { base4 }) => base4.inverse('email', 'Section', 'deployment.$id', deployment._id),
   },
 
   /**
