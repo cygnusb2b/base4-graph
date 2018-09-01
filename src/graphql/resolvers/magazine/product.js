63c54dfd4e815810b6e9a882246a9105db510750
Jacob Bare <jacob@limit0.io>
2018-09-01 14:28:38 -0500

    Add directives and inputs to magazine publication

--- src/graphql/resolvers/magazine/product.js
+++ src/graphql/resolvers/magazine/product.js
@@ -1,20 +1,11 @@
 const productResolvers = require('../platform/product/common');
 
-const { isArray } = Array;
-
 module.exports = {
   /**
    *
    */
   MagazineProductPublication: {
     ...productResolvers,
-    issues: (pub, _, { base4 }) => base4.inverse('magazine', 'Issue', 'publication.$id', pub._id),
-    sections: (pub, _, { base4 }) => base4.inverse('magazine', 'Section', 'publication.$id', pub._id),
-    coverImage: (pub, _, { base4 }) => base4.reference('platform', 'Asset', pub.coverImage, { type: 'Image' }),
-    socialLinks: (site) => {
-      const { socialLinks } = site;
-      return isArray(socialLinks) ? socialLinks : [];
-    },
   },
 
   /**
