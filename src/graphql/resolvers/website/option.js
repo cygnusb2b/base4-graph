989334a5ac9ec632a3a0f083adaf643cd59665a7
Jacob Bare <jacob@limit0.io>
2018-09-01 15:27:01 -0500

    Add websiteOptions query

--- src/graphql/resolvers/website/option.js
+++ src/graphql/resolvers/website/option.js
@@ -1,4 +1,6 @@
+const { ObjectID } = require('mongodb');
 const paginationResolvers = require('../../../pagination/resolvers');
+const formatStatus = require('../../../utils/format-graph-status');
 
 module.exports = {
   /**
@@ -17,6 +19,23 @@ module.exports = {
       auth.check();
       const { id } = input;
       return base4.strictFindById('website', 'Option', id);
+    },
+
+    /**
+     *
+     */
+    websiteOptions: async (_, { input }, { auth, base4 }) => {
+      auth.check();
+      const {
+        status,
+        siteId,
+        sort,
+        pagination,
+      } = input;
+
+      const criteria = { ...formatStatus(status) };
+      if (siteId) criteria['site.$id'] = new ObjectID(siteId);
+      return base4.paginate('website.Option', { pagination, sort, criteria });
     },
   },
 };
