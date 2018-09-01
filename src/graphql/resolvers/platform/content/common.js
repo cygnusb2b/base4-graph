8d5e160cc45465408479e25b359c42c51b6ad906
Jacob Bare <jacob@limit0.io>
2018-09-01 11:53:46 -0500

    Use directives for primarySite/Section and users

--- src/graphql/resolvers/platform/content/common.js
+++ src/graphql/resolvers/platform/content/common.js
@@ -3,14 +3,4 @@ const { extractMutationValue } = require('../../../../base4');
 module.exports = {
   primaryImage: (doc, _, { base4 }) => base4.reference('platform', 'Asset', doc.primaryImage),
   taxonomy: (doc, _, { base4 }) => base4.references('platform', 'Taxonomy', doc.taxonomy),
-  primarySite: async (doc, _, { base4 }) => {
-    const primarySite = extractMutationValue(doc, 'Website', 'primarySite');
-    return base4.reference('platform', 'Product', primarySite, { type: 'Site' });
-  },
-  primarySection: (doc, _, { base4 }) => {
-    const primarySection = extractMutationValue(doc, 'Website', 'primarySection');
-    return base4.reference('website', 'Section', primarySection);
-  },
-  createdBy: (doc, _, { base4 }) => base4.reference('platform', 'User', doc.createdBy),
-  updatedBy: (doc, _, { base4 }) => base4.reference('platform', 'User', doc.updatedBy),
 };
