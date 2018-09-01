87813fadf74c166333204208dc872e07eea2ea5a
Jacob Bare <jacob@limit0.io>
2018-09-01 14:20:26 -0500

    Add directives and inputs to magazine issue

--- src/graphql/resolvers/magazine/issue.js
+++ src/graphql/resolvers/magazine/issue.js
@@ -1,19 +1,4 @@
-const { isArray } = Array;
-
 module.exports = {
-  /**
-   *
-   */
-  MagazineIssue: {
-    publication: (issue, _, { base4 }) => base4.reference('platform', 'Product', issue.publication, { type: 'Publication' }),
-    sections: (issue, _, { base4 }) => base4.inverse('magazine', 'Section', 'issue.$id', issue._id),
-    coverImage: (issue, _, { base4 }) => base4.reference('platform', 'Asset', issue.coverImage, { type: 'Image' }),
-    redirects: (section) => {
-      const { redirects } = section;
-      return isArray(redirects) ? redirects : [];
-    },
-  },
-
   /**
    *
    */
