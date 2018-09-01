3e2de819658a3bbc8451e562affd072f9f244e8b
Jacob Bare <jacob@limit0.io>
2018-09-01 13:48:48 -0500

    Add directives and inputs to website schedule

--- src/graphql/resolvers/website/schedule.js
+++ src/graphql/resolvers/website/schedule.js
@@ -1,18 +1,4 @@
-const scheduleResolvers = require('../platform/schedule/common');
-
 module.exports = {
-  /**
-   *
-   */
-  WebsiteSchedule: {
-    ...scheduleResolvers,
-    product: (doc, _, { base4 }) => base4.reference('platform', 'Product', doc.product, { type: 'Site' }),
-    section: (doc, _, { base4 }) => base4.reference('website', 'Section', doc.section),
-    option: (doc, _, { base4 }) => base4.reference('website', 'Option', doc.option),
-    categories: (doc, _, { base4 }) => base4.references('platform', 'Taxonomy', doc.categories, { type: 'Category' }),
-    primarySection: (doc, _, { base4 }) => base4.reference('website', 'Section', doc.primarySection),
-  },
-
   /**
    *
    */
