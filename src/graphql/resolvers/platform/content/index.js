1d42dacda030bad301c2fe8d30e1534539873595
Jacob Bare <jacob@limit0.io>
2018-09-04 13:44:03 -0500

    Add articles and published articles query

--- src/graphql/resolvers/platform/content/index.js
+++ src/graphql/resolvers/platform/content/index.js
@@ -2,12 +2,14 @@ const deepAssign = require('deep-assign');
 const paginationResolvers = require('../../../../pagination/resolvers');
 
 const apparatus = require('./apparatus');
+const article = require('./article');
 const contact = require('./contact');
 const inQuarters = require('./in-quarters');
 const product = require('./product');
 
 module.exports = deepAssign(
   apparatus,
+  article,
   contact,
   inQuarters,
   product,
