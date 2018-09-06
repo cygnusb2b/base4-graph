986dc5aff1b7b3b94d723783019ed8e75bc620b5
Jacob Bare <jacob@limit0.io>
2018-09-06 09:59:12 -0500

    Setup initial content query route
    For Base4 webhook handling

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
+const buildContentQuery = require('./build-content-query');
 const graphql = require('./graphql');
 const timings = require('./timings');
 
 module.exports = (app) => {
+  app.use('/build-content-query', buildContentQuery);
   app.use('/graphql', graphql);
   app.use('/timings', timings);
 };
