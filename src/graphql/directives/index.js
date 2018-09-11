69c4ec71945a4f0e90341667a21e71b3b9fa2997
Jacob Bare <jacob@limit0.io>
2018-09-11 15:37:41 -0500

    Create the @inflect directive

--- src/graphql/directives/index.js
+++ src/graphql/directives/index.js
@@ -1,6 +1,7 @@
 const ArrayValueDirective = require('./array-value');
 const ContentQueryAllDirective = require('./content-query-all');
 const ContentQueryOneDirective = require('./content-query-one');
+const InflectDirective = require('./inflect');
 const MomentFormatDirective = require('./moment-format');
 const MutatedValueDirective = require('./mutated-value');
 const PaginatedDirective = require('./paginated');
@@ -14,6 +15,7 @@ module.exports = {
   ArrayValueDirective,
   ContentQueryAllDirective,
   ContentQueryOneDirective,
+  InflectDirective,
   MomentFormatDirective,
   MutatedValueDirective,
   PaginatedDirective,
