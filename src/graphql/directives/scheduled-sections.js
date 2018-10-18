2772f6956682117b42ace2fedde64e997fe8c1bb
Josh Worden <solocommand@gmail.com>
2018-10-18 11:19:12 -0500

    Change to use b4.distinct

--- src/graphql/directives/scheduled-sections.js
+++ src/graphql/directives/scheduled-sections.js
@@ -8,8 +8,7 @@ class ScheduledSectionsDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) { // eslint-disable-line class-methods-use-this
     // eslint-disable-next-line no-param-reassign
     field.resolve = async ({ _id }, { input: { pagination, sort } }, { base4 }) => {
-      const coll = await base4.collection('website', 'Schedule');
-      const ids = await coll.distinct('section', { 'content.$id': _id });
+      const ids = await base4.distinct('website.Schedule', 'section', { 'content.$id': _id });
       const criteria = { _id: { $in: ids } };
       return base4.find('website.Section', {
         pagination,
